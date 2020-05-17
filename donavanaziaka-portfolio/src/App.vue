2bd01102c5cbd3eabed2b7813dfa65ee596ad663
Donavan <donavan.aziaka.1@ulaval.ca>
2020-05-17 00:37:39 -0400

    portfolio created

---
+++ donavanaziaka-portfolio/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <LandingPage/>
+  </div>
+</template>
+
+<script>
+  import LandingPage from "./pages/LandingPage";
+
+export default {
+  name: 'App',
+  components: {
+    LandingPage,
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ donavanaziaka-portfolio/src/components/Header.vue
@@ -0,0 +1,19 @@
+<template>
+    <header id="header">
+        <nav>
+            <a>Projects</a>
+            <a>About</a>
+            <a>Contact</a>
+        </nav>
+    </header>
+</template>
+
+<script>
+    export default {
+        name: "Header"
+    }
+</script>
+
+<style scoped>
+
+</style>
