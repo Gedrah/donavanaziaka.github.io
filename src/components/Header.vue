e1576daaf7aa10897fb559330a49cff93233258f
Donavan <donavan.aziaka.1@ulaval.ca>
2020-05-18 19:01:23 -0400

    portfolio rdy for the first version

---
+++ src/components/Header.vue
@@ -0,0 +1,26 @@
+<template>
+    <header v-if="lang" id="header">
+        <h1><a v-on:click="moveToElement('home')">{{this.lang.home}}</a></h1>
+        <nav>
+            <a v-on:click="moveToElement('about')">{{this.lang.about}}</a>
+            <a v-on:click="moveToElement('projects')">{{this.lang.projects}}</a>
+<!--            <a v-on:click="moveToElement('contact')">{{this.lang.contact}}</a>-->
+        </nav>
+    </header>
+</template>
+
+<script>
+    export default {
+        name: "Header",
+        props: {
+            moveToElement: Function,
+            lang: Object
+        }
+    }
+</script>
+
+<style scoped>
+    a {
+        cursor: pointer;
+    }
+</style>
